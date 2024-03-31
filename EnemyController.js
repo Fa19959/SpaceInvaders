d107d5c00492c1f8107b0414529bc5623f86e963
Fa19959 <fa6amyalmamari@gmail.com>
2024-04-01 00:29:17 +0400

    Added change to the createEnemy function

--- EnemyController.js
+++ EnemyController.js
@@ -1,3 +1,23 @@
 export default class EnemyController {
-    
+    enemyMap = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+      ];
+      enemyRows = [];
+    constructor(canvas){
+        this.canvas= canvas;
+        this.createEnemies();
+
+    }
+    draw(ctx){
+
+    }
+    createEnemies(){
+
+    }
+
 }
