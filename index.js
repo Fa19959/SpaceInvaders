e69ab19363d1b9192e5bdc5eaf70fae6b53fd62f
Fa19959 <fa6amyalmamari@gmail.com>
2024-03-31 22:17:01 +0400

    test the game loop

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("test");
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+canvas.width=600;
+canvas.height=600;
+
+const background = new Image();
+background.src ="images/space.png";
+
+function game(){
+
+}
+setInterval(game, 1000/60);
