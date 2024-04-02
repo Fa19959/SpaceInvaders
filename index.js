bb56bcb44cef8edd4c2204483912709d24589493
Fa19959 <fa6amyalmamari@gmail.com>
2024-04-03 03:28:16 +0400

    Changed bullet class

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import EnemyController from "./EnemyController.js";
+import Player from "./Player.js";
 
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
@@ -10,9 +11,12 @@ const background = new Image();
 background.src ="images/space.png";
 
 const enmyController = new EnemyController(canvas);
+const player = new Player(canvas, 3);
+
 
 function game(){
 ctx.drawImage(background,0,0,canvas.width,canvas.height);
-enmyController.draw(ctx);
+//enmyController.draw(ctx);
+player.draw(ctx);
 }
 setInterval(game, 1000/60);
