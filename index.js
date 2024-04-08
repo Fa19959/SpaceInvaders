8cbf0b6efb3337324555a277d3d9edb126fc55ac
Fa19959 <fa6amyalmamari@gmail.com>
2024-04-08 23:12:02 +0400

    Added comments

--- index.js
+++ index.js
@@ -16,21 +16,21 @@ const enemyBulletController = new BulletController(canvas, 4, "white", false);
 const enemyController = new EnemyController(canvas,enemyBulletController,playerBulletController
   );
   const player = new Player(canvas, 3, playerBulletController);
-let isGameOver = false;
-let didWin = false;
+//let isGameOver = false;
+//let didWin = false;
 
 function game() {
-    checkGameOver();
+   // checkGameOver();
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    displayGameOver();
-    if (!isGameOver) {
+   // displayGameOver();
+   // if (!isGameOver) {
       enemyController.draw(ctx);
       player.draw(ctx);
       playerBulletController.draw(ctx);
       enemyBulletController.draw(ctx);
-    }
+   // }
   }  
-function displayGameOver() {
+/*function displayGameOver() {
     if (isGameOver) {
       let text = didWin ? "You Win" : "Game Over";
       let textOffset = didWin ? 3.5 : 5;
@@ -39,9 +39,9 @@ function displayGameOver() {
       ctx.font = "70px Arial";
       ctx.fillText(text, canvas.width / textOffset, canvas.height / 2);
     }
-  }
+  }*/
 
-  function checkGameOver() {
+ /* function checkGameOver() {
     if (isGameOver) {
       return;
     }
@@ -58,6 +58,6 @@ function displayGameOver() {
       didWin = true;
       isGameOver = true;
     }
-  }
+  }*/
   
   setInterval(game, 1000 / 60);
